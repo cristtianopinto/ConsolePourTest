5592dc4cce51158e63955ac0445b923d91ead005
cris <cristtianopinto@gmail.com>
2018-10-08 11:03:08 +0200

    Changement de Prix Test

---
+++ ConsolePourTesteTestes/ProduitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ConsolePourTest.Entites;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsolePourTesteTestes
+{
+    [TestClass]
+    public class ProduitTest
+    {
+        [TestMethod]
+        public void ImpossibleDeMettrëPrixInfOuEgal0()
+        {
+            var exception = Assert.ThrowsException<Exception>(() =>
+            {
+                var produit = new Produit
+                {
+                    Nom = "Bouteille Bordeaux",
+                    Prix = 0
+                };
+            });
+            Assert.AreEqual("Prix ne peut pas être négatif", exception.Message);
+        }
+
+        [TestMethod]
+        public void ImpossibleDeMettreNomVide()
+        {
+            Assert.ThrowsException<Exception>(() =>
+            {
+                var produit = new Produit
+                {
+                    Nom = "",
+                    Prix = 0
+                };
+                produit.Valider();
+            });
+        }
+    }
+}
