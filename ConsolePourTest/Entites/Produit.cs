d051805149d193bedec8d9a87138a63344df983c
cris <cristtianopinto@gmail.com>
2018-10-08 11:02:29 +0200

    Validation des prix et Nom du Produit

--- ConsolePourTest/Entites/Produit.cs
+++ ConsolePourTest/Entites/Produit.cs
@@ -8,8 +8,30 @@ namespace ConsolePourTest.Entites
 {
     public class Produit
     {
+        private decimal prix;
+
         public int Id { get; set; }
+
         public string Nom { get; set; }
-        public decimal Prix { get; set; }    
+
+        
+        public decimal Prix
+        {
+            get { return this.prix; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Prix ne peut pas être négatif");
+                prix = value;
+            }
+        }
+
+        public void Valider()
+        {
+            if(Prix <= 0)
+                throw new Exception("Prix ne peut pas être négatif");
+            if (string.IsNullOrEmpty(Nom))
+                throw new Exception("Nom est requis");
+        }
     }
 }
